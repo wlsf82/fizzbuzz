bf189f20a3a41148cb0c9aeeaa386647d17c2b6d
Walmyr <wlsf82@gmail.com>
2023-11-02 21:57:36 +0100

    Create project structure

---
+++ src/test.index.js
@@ -0,0 +1,10 @@
+/**
+ * FizzBuzz
+ *
+ * Para os números de 1 a 100,
+ *
+ * se o número for divisível por 3 imprima Fizz;
+ * se o número for divisível por 5, imprima Buzz;
+ * se o número for divisível por 3 e 5 (ex.: 15) imprima o FizzBuzz;
+ * caso contrário, imprima o número.
+ */
